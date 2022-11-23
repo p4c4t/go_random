349869af1ed3ca5ff6c5b5bea36eb064dd65d5fb
p4c4t <vsmorodov+github@protonmail.com>
2022-11-23 12:33:36 +0300

    Added the ability to read type and value from stdin

--- generic_sqrt.go
+++ generic_sqrt.go
@@ -1,25 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"math"
+  "fmt"
 )
 
 type number interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+	int | uint | float32 | float64
 }
 
 func Sqrt[T number](x T) T {
 	var z T
 	z = 1.0
 	for i := 0; z != 0 && i < 10; i += 1 {
-		fmt.Println(i, z)
+		//fmt.Println(i, z)
 		z = z - (z*z-x)/(2*z)
 	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt[int](763), math.Sqrt(763))
+	var type_str string
+	for {
+		fmt.Scanln(&type_str)
+		switch(type_str) {
+			case "int":
+				var x int
+				fmt.Scanln(&x)
+				fmt.Println(Sqrt[int](x))
+			case "uint":
+				var x uint
+				fmt.Scanln(&x)
+				fmt.Println(Sqrt[uint](x))
+			case "float32":
+				var x float32
+				fmt.Scanln(&x)
+				fmt.Println(Sqrt[float32](x))
+			case "float64":
+				var x float64
+				fmt.Scanln(&x)
+				fmt.Println(Sqrt[float64](x))
+			default:
+				fmt.Println("error")
+		}
+	}
 }
 

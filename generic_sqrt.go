76fede8f367452b6c47a6303e442b1e78d0b3117
p4c4t <vsmorodov+github@protonmail.com>
2022-11-24 21:58:40 +0300

    Added support for complex numbers, tidied up the code and changed the approximation algorithm slightly

--- generic_sqrt.go
+++ generic_sqrt.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type number interface {
-	int | uint | float32 | float64
+	int | uint | float32 | float64 | complex64 | complex128
 }
 
 func Sqrt[T number](x T) T {
 	var z T
-	z = 1.0
-	for i := 0; z != 0 && i < 10; i += 1 {
-		//fmt.Println(i, z)
-		z = z - (z*z-x)/(2*z)
+	z = x
+	for i := 0; i < 100; i += 1 {
+		fmt.Println(i, z)
+		z = (z + x/z) / 2
 	}
 	return z
 }
@@ -22,26 +22,33 @@ func main() {
 	var type_str string
 	for {
 		fmt.Scanln(&type_str)
-		switch(type_str) {
-			case "int":
-				var x int
-				fmt.Scanln(&x)
-				fmt.Println(Sqrt[int](x))
-			case "uint":
-				var x uint
-				fmt.Scanln(&x)
-				fmt.Println(Sqrt[uint](x))
-			case "float32":
-				var x float32
-				fmt.Scanln(&x)
-				fmt.Println(Sqrt[float32](x))
-			case "float64":
-				var x float64
-				fmt.Scanln(&x)
-				fmt.Println(Sqrt[float64](x))
-			default:
-				fmt.Println("error")
+		switch type_str {
+		case "int":
+			var x int
+			fmt.Scanln(&x)
+			fmt.Println(Sqrt(x))
+		case "uint":
+			var x uint
+			fmt.Scanln(&x)
+			fmt.Println(Sqrt(x))
+		case "float32":
+			var x float32
+			fmt.Scanln(&x)
+			fmt.Println(Sqrt(x))
+		case "float64":
+			var x float64
+			fmt.Scanln(&x)
+			fmt.Println(Sqrt(x))
+		case "complex64":
+			var x complex64
+			fmt.Scanln(&x)
+			fmt.Println(Sqrt(x))
+		case "complex128":
+			var x complex128
+			fmt.Scanln(&x)
+			fmt.Println(Sqrt(x))
+		default:
+			fmt.Println("error")
 		}
 	}
 }
-
